a18deb7e8c65f4d908f1c7c943715591c1319815
Dhuvarakesan Mani <dhuvarakesan.mani@expleogroup.com>
2025-08-23 00:48:34 +0530

    server check call changed to public access

--- server/src/server.ts
+++ server/src/server.ts
@@ -35,24 +35,9 @@ app.use('/products', protect, productRoutes);
 // Add transaction routes
 app.use('/transactions', protect,transactionRoutes);
 // Root route
-app.get("/", (req: Request, res: Response) => {
-  res.send(`
+// app.get("/", (req: Request, res: Response) => {
   
-    <div style="display: flex; justify-content: center; align-items: center; height:100%; text-align: center; font-family: Arial, sans-serif; background-color: #f8f9fa;">
-      <pre style="font-size: 18px; font-weight: bold; color: #333; padding: 20px; border: 1px solid #ddd; border-radius: 10px; background-color: #ffffff; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);">
-🎉 Welcome to the Inventory Management Server! 🎉
---------------------------------------------
-🚀 The server is up and running smoothly!
-🌐 Current Status: ONLINE
-🟢 Port: ${port}
---------------------------------------------
-Thank you for using our service! 😊
-
-      </pre>
-    </div>
-   
-  `);
-});
+// });
 
 
 
